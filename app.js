03603fa71b8d7a1e58f1a63e7764f42b1d677ee7
Abhijeet Kulkarni <abhijeetkulkarni168@gmail.com>
2024-03-14 22:57:46 +0530

    ROCK PAPER SCISSORS GAME

---
+++ app.js
@@ -0,0 +1,94 @@
+let userScore = 0;
+let compScore = 0;
+
+let choices = document.querySelectorAll(".choice");
+
+const msg = document.querySelector("#msg");
+
+let userScorePara = document.querySelector("#user-score");
+let compScorePara = document.querySelector("#comp-score");
+
+
+
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"]
+    // rock paper scissors
+    const randInd = Math.floor(Math.random() * 3);
+    return options[randInd];
+
+}
+
+
+
+const drawGame = () => {
+
+    msg.innerText = "Game Was Draw. Play Again.";
+    msg.style.backgroundColor = "#081b31";
+}
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Win!  Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+
+    }
+    else {
+        compScore++;
+        compScorePara.innerText = compScore;
+
+        msg.innerText = `You lose ${compChoice} beats Your ${userChoice}`
+        msg.style.backgroundColor = "red";
+    }
+}
+
+
+
+const playGame = (userChoice) => {
+
+    // generate computer choice -> modular Way of programming
+    const compChoice = genCompChoice();
+
+
+    if (userChoice === compChoice) {
+        // draw game 
+        drawGame();
+    }
+    else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            // scissors , paper 
+            userWin = compChoice === "paper" ? false : true;
+        }
+        else if (userChoice === "paper") {
+            // rock scissors 
+            userWin = compChoice === "scissors" ? false : true;
+        }
+        else {
+            // rock paper 
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+}
+
+
+choices.forEach((choice) => {
+    /* console.log(choice); */
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        // console.log( userChoice, "choice was clicked !") ;
+
+        playGame(userChoice);
+    })
+})
+
+
+
+
+
+
+
+
